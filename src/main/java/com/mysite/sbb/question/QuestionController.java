e7d571b848354b0a15300810d13f6ea79c381162
sik2 <bickwang123@gmail.com>
2025-08-20 10:02:42 +0900

    2-17 3rd

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,9 +1,14 @@
 package com.mysite.sbb.question;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -37,9 +42,13 @@ public class QuestionController {
     }
 
     @PostMapping("/create")
-    public String createQuestion(@RequestParam("subject") String subject,  @RequestParam("content") String content) {
+    public String createQuestion(@Valid QuestionForm questionForm, BindingResult bindingResult) {
 
-        questionService.create(subject, content);
+        if(bindingResult.hasErrors()) {
+            return "question_form";
+        }
+
+        questionService.create(questionForm.getSubject(), questionForm.getContent());
 
         return "redirect:/question/list";
     }

e2cecdf84aeefaf06a2223daaf5c46a512db7208
sik2 <bickwang123@gmail.com>
2025-08-20 10:44:57 +0900

    2-17 5rd

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -2,11 +2,13 @@ package com.mysite.sbb.answer;
 
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @Controller
@@ -15,10 +17,15 @@ public class AnswerController {
     private final QuestionService questionService;
 
     @PostMapping("/answer/create/{id}")
-    public String create(@PathVariable Integer id, @RequestParam("content") String content) {
+    public String create(Model model, @PathVariable Integer id, @Valid AnswerForm answerForm, BindingResult bindingResult) {
         Question question = questionService.getQuestion(id);
 
-        answerService.create(question, content);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
+
+        answerService.create(question, answerForm.getContent());
 
         return "redirect:/question/detail/%d".formatted(id);
     }
